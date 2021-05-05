4cb2fdbed7eeb32022185caaedbc21089da456d4
Your Name <you@example.com>
2021-05-04 23:28:29 -0500

    meta for twitter

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,14 @@ export default function Home() {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
+        <meta property="og:image" content="../assets/preview.png" />
+        <meta name="twitter:creator" content="@SovereignKiche" />
+        <meta property="og:title" content="Crypto Currency Rankings" />
+        <meta
+          property="og:description"
+          content="Vote on your favorite cryptocurrencies and submit them to the global rankings. See how your top ten compare to the global top ten crypto currencies which are based on the peoples votes!"
+        />
+        <meta name="twitter:card" content="summary"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
